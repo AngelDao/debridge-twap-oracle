4e5c165759ff2715b6db1967759842fe27afcaf8
meirbank <meir@meirbank.com>
2022-05-07 01:07:29 -0400

    fix chain ids

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,10 +1,11 @@
-const bscChainId = 97;
 const polygonChainId = 137;
+const bscChainId = 97;
 
-export const FROM_CHAIN_ID = bscChainId;
-export const TO_CHAIN_ID = polygonChainId;
-export const DE_BRIDGE_GATE_ADDRESS = '0x43dE2d77BF8027e25dBD179B491e8d64f38398aA';
+
+export const FROM_CHAIN_ID = polygonChainId;
+export const TO_CHAIN_ID = bscChainId;
+export const DE_BRIDGE_GATE_ADDRESS = '0x43dE2d77BF8027e25dBD179B491e8d64f38398aA'; // same address for all the current chains
 
 // Set after deploy
-export const ADDRESS_ON_FROM = '0x51487Af4A13a60188EE47595023Cc42Ba943F2F7';
-export const ADDRESS_ON_TO = '0x423913626649997532712e129277CF152c642e39'
+export const ADDRESS_ON_FROM = '0xc4FE0dc6B0E7B3BfA6346D1E843377075E2C187B';
+export const ADDRESS_ON_TO = '0xBB95Ed61649C62C67260B79A17d865c1C2c12764'
